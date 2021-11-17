8f6efd57549f17b4c89a00a3ca8e4dcfafc93a6c
Ivan Ievlev <ivanievlev@gmail.com>
2021-11-17 16:12:16 +0300

    Added skewed values for warehouse and inventory

--- 04_load/009.gpdb.warehouse.sql
+++ 04_load/009.gpdb.warehouse.sql
@@ -1,2 +1,18 @@
 TRUNCATE table tpcds.warehouse;
 INSERT INTO tpcds.warehouse SELECT * FROM ext_tpcds.warehouse;
+insert into tpcds.warehouse select
+777 as w_warehouse_sk
+,'LUKAAAAAAAAAAAAA' as w_warehouse_id
+,'Luka WH' as w_warehouse_name
+,w_warehouse_sq_ft
+,w_street_number
+,w_street_name
+,w_street_type
+,w_suite_number	
+,w_city 	
+,w_county
+,w_state
+,w_zip
+,w_country
+,w_gmt_offset
+from tpcds.warehouse where w_warehouse_sk = 1;
--- 04_load/018.gpdb.inventory.sql
+++ 04_load/018.gpdb.inventory.sql
@@ -1,2 +1,3 @@
 TRUNCATE table tpcds.inventory;
 INSERT INTO tpcds.inventory SELECT * FROM ext_tpcds.inventory;
+update tpcds.inventory set inv_warehouse_sk = 777 where inv_warehouse_sk < 10;
